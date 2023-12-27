47b359518c6db59227b5c352d1cb3a647958593e
phelipececilio <phelipe.cecilio@gmail.com>
2023-12-26 23:34:11 -0300

    add teste cpf

--- validacpf/src/test/java/br/com/gilmarioarantes/validacpf/model/ValidaCPFTeste.java
+++ validacpf/src/test/java/br/com/gilmarioarantes/validacpf/model/ValidaCPFTeste.java
@@ -1,10 +1,47 @@
 package br.com.gilmarioarantes.validacpf.model;
 
-import static org.testng.Assert.*;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class ValidaCPFTeste {
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
-    
+public class ValidaCPFTeste {
+    private ValidaCPF validador;
+
+    @BeforeTest
+    public void setUp() {
+        validador = new ValidaCPF();
+    }
+
+    @Test
+    public void testCPFValido() {
+        assertTrue(validador.isCPF("12345678909"));
+    }
+
+    @Test
+    public void testCPFInvalido() {
+        assertFalse(validador.isCPF("12345678900"));
+    }
+
+    @Test
+    public void testCPFComCaracteresNaoNumericos() {
+        assertFalse(validador.isCPF("12345x7890z"));
+    }
+
+    @Test
+    public void testCPFMuitoLongo() {
+        assertFalse(validador.isCPF("1234567890123"));
+    }
+
+    @Test
+    public void testCPFMuitoCurto() {
+        assertFalse(validador.isCPF("12345"));
+    }
+
+    @Test
+    public void testCPFComTodosDigitosIguais() {
+        assertFalse(validador.isCPF("11111111111"));
+    }
+
 }
