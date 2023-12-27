c7d271deef630185b8e058ca7cdadc23f7ee5e9c
Marcos Vinícius de Moraes <viniciusmarcos@discente.ufg.br>
2023-12-27 17:43:27 -0300

    Triangulo - alteração de nomes

--- triangulo/src/test/java/br/com/gilmario/TrianguloTest.java
+++ triangulo/src/test/java/br/com/gilmario/TrianguloTest.java
@@ -19,7 +19,7 @@ public class TrianguloTest {
     }
 
     @Test
-    public void validaCMaiorAB() throws TrianguloException {
+    public void invalidaCMaiorAB() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(2,2,5));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
@@ -31,7 +31,7 @@ public class TrianguloTest {
     }
 
     @Test
-    public void validaBMaiorAC() throws TrianguloException {
+    public void invalidaBMaiorAC() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(2,4,1));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
@@ -43,35 +43,35 @@ public class TrianguloTest {
     }
 
     @Test
-    public void validaAMaiorBC() throws TrianguloException {
+    public void invalidaAMaiorBC() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(10,2,5));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
     }
 
     @Test
-    public void validaLado0() throws TrianguloException {
+    public void invalidaLado0() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(0, 1, 2));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
     }
 
     @Test
-    public void validaLadoNegativo() throws TrianguloException {
+    public void invalidaLadoNegativo() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(-1, 1, 2));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
     }
 
     @Test
-    public void validaAIqualBC() throws TrianguloException {
+    public void invalidaAIqualBC() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(3, 1, 2));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
     }
 
     @Test
-    public void valida3Lados0() throws TrianguloException {
+    public void invalida3Lados0() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(0, 0, 0));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
@@ -135,7 +135,7 @@ public class TrianguloTest {
     //Teste não chega a compilar devido ao tamanho do número C
     /*
     @Test
-    public void validaMaxint() throws TrianguloException {
+    public void invalidaMaxint() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(1431655765, 1431655765, 2147483648));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
@@ -145,7 +145,7 @@ public class TrianguloTest {
     //Teste não chega a compilar devido ao parâmetro não integer
     /*
     @Test
-    public void validaNaoInteger() throws TrianguloException {
+    public void invalidaNaoInteger() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(a, 2, 3));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
@@ -155,7 +155,7 @@ public class TrianguloTest {
     //Teste não chega a compilar devido a falta de parâmetros suficiente
     /*
     @Test
-    public void valida2Parametros() throws TrianguloException {
+    public void invalida2Parametros() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(1, 2));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
@@ -165,7 +165,7 @@ public class TrianguloTest {
     //Teste não chega a compilar devido ao excesso de parâmetros
     /*
     @Test
-    public void valida4Parametros() throws TrianguloException {
+    public void invalida4Parametros() throws TrianguloException {
         Exception exception = assertThrows(TrianguloException.class, () ->
                 t.validaTriangulo(1, 2, 3, 4));
         assertEquals("Os valores informados não formam um triangulo!", exception.getMessage());
